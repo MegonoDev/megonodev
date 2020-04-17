2ab84ef8c8953d42613c8d6c2ab2d9e205f54174
ferifahrul13 <ferifahrul1@gmail.com>
2020-04-18 04:04:42 +0700

    finish templating

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,34 @@
 
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+
+Auth::routes(['register' => false, 'password.request' => false]); //disable karena mau custom login
+Route::get('/login', function () {
+    return view('errors.404');
+});
+Route::post('/login', function () {
+    return view('errors.404');
+});
+Route::get('/logout', function () {
+    return view('errors.404');
+});
+
+Route::get('/admin', function () {
+    return redirect('/admin/home');
+});
+Route::post('/login', function () {
+    return view('errors.404');
+});
+
+Route::group(['namespace' => 'Auth'], function () {
+    Route::get('/admin/login', 'LoginController@showLoginForm')->name('login');
+    Route::post('/admin/login', 'LoginController@login');
+    Route::post('/admin/logout', 'LoginController@logout')->name('logout');
+});
+
+
+Route::group(['namespace' => 'b', 'prefix' => 'admin'], function () {
+    Route::get('home', 'HomeController@index')->name('home');
 });
