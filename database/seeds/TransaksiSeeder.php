ca3ad508ec4b655974ebdcd3133809ca19ed46bd
virusphp <packerycber@gmail.com>
2020-04-22 19:22:55 +0700

    seeder transaksi header

--- database/seeds/TransaksiSeeder.php
+++ database/seeds/TransaksiSeeder.php
@@ -12,8 +12,9 @@ class TransaksiSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;'); 
         DB::table('transaksi_headers')->truncate();
-        
+        // DB::statement('SET FOREIGN_KEY_CHECKS=1;'); 
         $transaksi = [];
         $faker = Faker\Factory::create();
         $date = Carbon::create(2020, 3, 7, 24);
@@ -22,17 +23,19 @@ class TransaksiSeeder extends Seeder
             $date->now();
             $createDate = clone ( $date );
 
-            $nama = ['Fery','gandi','rori'];
 
             $transaksi[] = [
                 'invoice' => 'KL' . $i,
-                'nama'  => rand($nama),
+                'nama'  =>  $faker->name,
                 'total_harga' => 10000,
                 'pajak' => 10,
                 'keterangan' => 'Fresh pembelian perangkat kerja',
                 'id_user' => 1,
-                'id_akun' => 2,
+                'id_akun' => 1, 
+                'created_at' => $createDate
             ];
         }
+
+        DB::table('transaksi_headers')->insert($transaksi);
     }
 }
