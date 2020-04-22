3fd402574c5f105dae54d28dba54ccedbce58ee7
Slamet Sugandi <packercyber@gmail.com>
2020-04-22 15:37:55 +0700

    make seeder blom selesai

--- database/seeds/TransaksiSeeder.php
+++ database/seeds/TransaksiSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class TransaksiSeeder extends Seeder
@@ -11,6 +12,27 @@ class TransaksiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('transaksi_headers')->truncate();
+        
+        $transaksi = [];
+        $faker = Faker\Factory::create();
+        $date = Carbon::create(2020, 3, 7, 24);
+
+        for($i = 0; $i<=10; $i++) {
+            $date->now();
+            $createDate = clone ( $date );
+
+            $nama = ['Fery','gandi','rori'];
+
+            $transaksi[] = [
+                'invoice' => 'KL' . $i,
+                'nama'  => rand($nama),
+                'total_harga' => 10000,
+                'pajak' => 10,
+                'keterangan' => 'Fresh pembelian perangkat kerja',
+                'id_user' => 1,
+                'id_akun' => 2,
+            ];
+        }
     }
 }
