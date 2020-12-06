8128e1f2f57aa3e79d589bd8ae4d9d80bcb2c471
Revangga007 <revanggaajip2@gmail.com>
2020-12-06 22:48:33 +0700

    Membuat portfolio model, controller, dan migration

---
+++ database/migrations/2020_12_06_222853_create_portfolios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('slug');
+            $table->text('keterangan');
+            $table->string('thumbnail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
